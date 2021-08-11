5aabc4ee93fcaa2a6d74d1ad93a779af687709f0
donghyeon0725 <ehdgus5015@gmail.com>
2021-08-11 22:03:17 +0900

    Initial commit

---
+++ src/main/java/today/what_should_i_eat_today/WhatShouldIEatTodayApplication.java
@@ -0,0 +1,13 @@
+package today.what_should_i_eat_today;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatShouldIEatTodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatShouldIEatTodayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/today/what_should_i_eat_today/WhatShouldIEatTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package today.what_should_i_eat_today;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatShouldIEatTodayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
