a0d8e6e68917032a91fa756665305feb3c82ce38
donghyeon0725 <ehdgus5015@gmail.com>
2021-08-12 17:05:46 +0900

    Fix nested bean Exception

--- src/main/java/today/what_should_i_eat_today/domain/food/api/FoodRankingApi.java
+++
@@ -1,9 +0,0 @@
-package today.what_should_i_eat_today.domain.food.api;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-public class FoodRankingApi {
-}
--- src/main/java/today/what_should_i_eat_today/domain/food/application/FoodRankingService.java
+++
@@ -1,9 +0,0 @@
-package today.what_should_i_eat_today.domain.food.application;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class FoodRankingService {
-}
