6a1085f31ebd2f8f2a9e2a00251f3ca46d3d1e0b
donghyeon0725 <ehdgus5015@gmail.com>
2021-08-13 16:47:52 +0900

    Fix add "@Transactional(readOnly = true)" to all Service class

--- src/main/java/today/what_should_i_eat_today/domain/category/application/CategoryService.java
+++ src/main/java/today/what_should_i_eat_today/domain/category/application/CategoryService.java
@@ -2,8 +2,10 @@ package today.what_should_i_eat_today.domain.category.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CategoryService {
 }
