37e649a83f1c3e4fa6670ed98ff6ab06f523d741
donghyeon0725 <ehdgus5015@gmail.com>
2021-08-18 18:47:22 +0900

    Feat Course CRUD Complete

--- src/main/java/today/what_should_i_eat_today/domain/world_cup/entity/QuestionValidator.java
+++ src/main/java/today/what_should_i_eat_today/domain/world_cup/entity/QuestionValidator.java
@@ -2,12 +2,14 @@ package today.what_should_i_eat_today.domain.world_cup.entity;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import today.what_should_i_eat_today.domain.world_cup.dao.QuestionPackageRepository;
 import today.what_should_i_eat_today.global.error.ErrorCode;
 import today.what_should_i_eat_today.global.error.exception.CannotExecuteException;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class QuestionValidator {
     private final QuestionPackageRepository questionPackageRepository;
 
