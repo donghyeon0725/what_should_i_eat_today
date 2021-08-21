9023b83f68d898d6fa8893d437f036b01fe4cb83
hbh <sout1217@naver.com>
2021-08-21 18:06:54 +0900

    feat Post Like Update

--- src/main/java/today/what_should_i_eat_today/domain/post/application/PostService.java
+++ src/main/java/today/what_should_i_eat_today/domain/post/application/PostService.java
@@ -3,9 +3,44 @@ package today.what_should_i_eat_today.domain.post.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import today.what_should_i_eat_today.domain.likes.dao.LikesRepository;
+import today.what_should_i_eat_today.domain.likes.entity.Likes;
+import today.what_should_i_eat_today.domain.member.dao.MemberRepository;
+import today.what_should_i_eat_today.domain.member.entity.Member;
+import today.what_should_i_eat_today.domain.post.dao.PostRepository;
+import today.what_should_i_eat_today.domain.post.entity.Post;
+import today.what_should_i_eat_today.global.error.ErrorCode;
+import today.what_should_i_eat_today.global.error.exception.ResourceNotFoundException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostService {
+
+    private final MemberRepository memberRepository;
+    private final LikesRepository likesRepository;
+    private final PostRepository postRepository;
+
+
+    @Transactional
+    public boolean updateLike(Long postId, Long userId) {
+
+        Member member = memberRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(ErrorCode.RESOURCE_NOT_FOUND));
+
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException(ErrorCode.RESOURCE_NOT_FOUND));
+
+        Optional<Likes> postLike = likesRepository.findByPostAndMember(post, member);
+
+        if (postLike.isPresent()) {
+            post.removeLike(postLike.get());
+            return false;
+        } else {
+            Likes newPostLike = Likes.builder().member(member).post(post).build();
+            post.addLike(newPostLike);
+            likesRepository.save(newPostLike);
+            return true;
+        }
+    }
 }
