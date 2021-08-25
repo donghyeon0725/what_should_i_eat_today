56c7777871920b0648523b5c74c6647275e31877
donghyeon0725 <ehdgus5015@gmail.com>
2021-08-25 09:03:06 +0900

    Feat Events Publisher

---
+++ src/main/java/today/what_should_i_eat_today/event/event/DomainEvent.java
@@ -0,0 +1,13 @@
+package today.what_should_i_eat_today.event.event;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DomainEvent {
+    private Object object;
+}
