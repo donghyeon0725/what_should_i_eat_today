7e8ee5be8cbd817e090c70192692f621a01b9aaf
hbh <sout1217@naver.com>
2021-08-27 15:16:41 +0900

    feat get My Favorite Posts

--- src/main/java/today/what_should_i_eat_today/domain/favorite/dao/FavoriteRepository.java
+++ src/main/java/today/what_should_i_eat_today/domain/favorite/dao/FavoriteRepository.java
@@ -3,5 +3,8 @@ package today.what_should_i_eat_today.domain.favorite.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import today.what_should_i_eat_today.domain.favorite.entity.Favorite;
 
+import java.util.List;
+
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    List<Favorite> findAllByMemberId(Long memberId);
 }
