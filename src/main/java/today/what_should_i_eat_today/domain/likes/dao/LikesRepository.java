fa01650fcaf684b3a2e9d74ec07205d422fbf672
hbh <sout1217@naver.com>
2021-08-27 17:11:02 +0900

    feat get My Likes Posts

--- src/main/java/today/what_should_i_eat_today/domain/likes/dao/LikesRepository.java
+++ src/main/java/today/what_should_i_eat_today/domain/likes/dao/LikesRepository.java
@@ -5,9 +5,12 @@ import today.what_should_i_eat_today.domain.likes.entity.Likes;
 import today.what_should_i_eat_today.domain.member.entity.Member;
 import today.what_should_i_eat_today.domain.post.entity.Post;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 
     Optional<Likes> findByPostAndMember(Post post, Member member);
+
+    List<Likes> findAllByMemberId(Long memberId);
 }
