75e44b11c03f7b95c063d418e1edfc7cc3d79021
hbh <sout1217@naver.com>
2021-08-30 17:24:15 +0900

    feat search foods by category

--- src/main/java/today/what_should_i_eat_today/domain/food/api/FoodApi.java
+++ src/main/java/today/what_should_i_eat_today/domain/food/api/FoodApi.java
@@ -1,9 +1,29 @@
 package today.what_should_i_eat_today.domain.food.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import today.what_should_i_eat_today.domain.food.application.FoodCategoryService;
+import today.what_should_i_eat_today.domain.food.entity.Food;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class FoodApi {
+
+    private final FoodCategoryService foodCategoryService;
+
+    @GetMapping("/foods/categories/{id}")
+    public ResponseEntity<?> getFoodsInCategory(@PageableDefault Pageable pageable, @PathVariable("id") Long categoryId) {
+
+        Page<Food> foods = foodCategoryService.getFoods(categoryId, pageable);
+
+        return ResponseEntity.ok(foods);
+    }
 }
---
+++ src/test/java/today/what_should_i_eat_today/domain/food/application/FoodCategoryServiceTest.java
@@ -0,0 +1,77 @@
+package today.what_should_i_eat_today.domain.food.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+import today.what_should_i_eat_today.domain.category.entity.Category;
+import today.what_should_i_eat_today.domain.category.entity.FoodCategory;
+import today.what_should_i_eat_today.domain.food.entity.Food;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class FoodCategoryServiceTest {
+
+    @Autowired
+    private FoodCategoryService foodCategoryService;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    @DisplayName("카테고리에 해당하는 음식들 조회")
+    void test1() {
+        // todo : 2021.08.30 왠지 나중에 문제 생길 것 같은 코드로 보임
+
+        Category category1 = Category.builder()
+                .name("밥")
+                .build();
+        em.persist(category1);
+
+        Category category2 = Category.builder()
+                .name("돈까스")
+                .build();
+        em.persist(category2);
+
+        for (int i = 0; i < 50; i++) {
+            Food food = Food.builder()
+                    .name("food" + i)
+                    .build();
+
+            em.persist(food);
+
+            if (i % 2 == 0) {
+                FoodCategory foodCategory = FoodCategory.builder().food(food).build();
+                category1.addFoodMapping(foodCategory);
+                em.persist(foodCategory);
+            }
+
+            if (i % 3 == 0) {
+                FoodCategory foodCategory = FoodCategory.builder().food(food).build();
+                category2.addFoodMapping(foodCategory);
+                em.persist(foodCategory);
+            }
+        }
+
+        em.clear();
+
+        PageRequest page = PageRequest.of(0, 10);
+        Page<Food> foods = foodCategoryService.getFoods(category1.getId(), page);
+
+        assertThat(foods.getContent()).hasSize(10);
+        assertThat(foods.getContent())
+                .extracting("name")
+                .containsExactly(
+                        "food0", "food2", "food4", "food6", "food8",
+                        "food10", "food12", "food14", "food16", "food18"
+                );
+    }
+
+}
