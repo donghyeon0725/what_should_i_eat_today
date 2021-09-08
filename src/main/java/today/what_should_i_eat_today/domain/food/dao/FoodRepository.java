197ed71eeaf3ba28d0e6ecdf75147dc389f66770
donghyeon0725 <ehdgus5015@gmail.com>
2021-09-08 16:33:23 +0900

    Feat Food CRUD Complete

--- src/main/java/today/what_should_i_eat_today/domain/food/dao/FoodRepository.java
+++ src/main/java/today/what_should_i_eat_today/domain/food/dao/FoodRepository.java
@@ -26,4 +26,10 @@ public interface FoodRepository extends JpaRepository<Food, Long>, FoodDslReposi
     @Query(value = "select ff.* from (SELECT row_number() over() as rnum, f.* FROM Food f where f.deleted = 'false') as ff where ff.rnum in :rows", nativeQuery = true)
     List<Food> findByRows(@Param("rows") List<Integer> rows);
 
+
+    boolean existsByName(String name);
+
+    @Query(value = "select case when count (f) > 0 then true else false end from Food f where f.name = :new and f.name <> :old")
+    boolean existsByName(@Param("old") String oldName, @Param("new") String newName);
+
 }
