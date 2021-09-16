56594a08f75665f3c1ad4150dc22ae1a53db7b08
donghyeon0725 <ehdgus5015@gmail.com>
2021-09-16 17:26:16 +0900

    Change Server Port And Allow Front Web Client
    서버 포트번호 8081로 변경(클라이언트가 8080 사용).
    CORS 설정도 동일하게 맞춤

--- src/main/java/today/what_should_i_eat_today/global/config/WebMvcConfig.java
+++ src/main/java/today/what_should_i_eat_today/global/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
