8382192dcf370aadc0da80088de5f6219cce70ff
donghyeon0725 <ehdgus5015@gmail.com>
2021-09-16 17:24:59 +0900

    Fix Spring Security Config
    기존 시스템은 토큰의 subject에 ID를 넣고 토큰을 발급한 뒤, 사용자를 로드하고 Member 레파지토리에 유저가 없으면 admin 에서 로드하는 식이었는데,
    토큰의 subject 에 Email을 넣고 토큰을 발급하는 방식으로 변경
    인증시 권한을 먼저 살피고 ROLE_ADMIN 권한 인증을 요구하면 Admin 레파지토리에서 ROLE_MEMBER 권한 인증을 요구하면 Member 레파지토리에서 사용자를
    로드합니다.

--- src/main/java/today/what_should_i_eat_today/global/security/CustomUserDetailsService.java
+++ src/main/java/today/what_should_i_eat_today/global/security/CustomUserDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import today.what_should_i_eat_today.domain.admin.dao.AdminRepository;
+import today.what_should_i_eat_today.domain.admin.entity.Admin;
 import today.what_should_i_eat_today.domain.member.dao.MemberRepository;
 import today.what_should_i_eat_today.domain.member.entity.Member;
 import today.what_should_i_eat_today.global.error.ErrorCode;
@@ -33,13 +34,14 @@ public class CustomUserDetailsService implements UserDetailsService {
         // member 와 admin 테이블에 같은 이메일이 존재하는 경우 문제가 생길 수 있다
         return optionalMember
                 .map(UserPrincipal::create)
-                .orElseGet(() -> UserPrincipal.create(
-                        adminRepository
-                                .findByEmail(email)
-                                .orElseThrow(() -> new ResourceNotFoundException(ErrorCode.RESOURCE_NOT_FOUND)
-                                )
-                        )
-                );
+                .orElse(null);
+//                .orElseGet(() -> UserPrincipal.create(
+//                        adminRepository
+//                                .findByEmail(email)
+//                                .orElseThrow(() -> new ResourceNotFoundException(ErrorCode.RESOURCE_NOT_FOUND)
+//                                )
+//                        )
+//                );
     }
 
     @Transactional
@@ -51,12 +53,33 @@ public class CustomUserDetailsService implements UserDetailsService {
         // member 와 admin 테이블에 같은 이메일이 존재하는 경우 문제가 생길 수 있다
         return optionalMember
                 .map(UserPrincipal::create)
-                .orElseGet(() -> UserPrincipal.create(
-                        adminRepository
-                                .findById(id)
-                                .orElseThrow(() -> new ResourceNotFoundException(ErrorCode.RESOURCE_NOT_FOUND)
-                                )
-                        )
-                );
+                .orElseGet(null);
+//                .orElseGet(() -> UserPrincipal.create(
+//                        adminRepository
+//                                .findById(id)
+//                                .orElseThrow(() -> new ResourceNotFoundException(ErrorCode.RESOURCE_NOT_FOUND)
+//                                )
+//                        )
+//                );
+    }
+
+    @Transactional
+    public UserDetails loadAdminById(Long id) {
+        Optional<Admin> optionalAdmin = adminRepository.findById(id);
+
+        return optionalAdmin
+                .map(UserPrincipal::create)
+                .orElseGet(null);
+    }
+
+    @Transactional
+    public UserDetails loadAdminByUsername(String email)
+            throws UsernameNotFoundException {
+
+        Optional<Admin> optionalAdmin = adminRepository.findByEmail(email);
+
+        return optionalAdmin
+                .map(UserPrincipal::create)
+                .orElse(null);
     }
 }
--- src/test/java/today/what_should_i_eat_today/global/security/TokenProviderTest.java
+++ src/test/java/today/what_should_i_eat_today/global/security/TokenProviderTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import today.what_should_i_eat_today.domain.admin.dao.AdminRepository;
+import today.what_should_i_eat_today.domain.admin.entity.Admin;
 import today.what_should_i_eat_today.domain.member.dao.MemberRepository;
 import today.what_should_i_eat_today.domain.member.entity.Member;
 import today.what_should_i_eat_today.domain.model.AuthProvider;
@@ -17,6 +19,8 @@ class TokenProviderTest {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private AdminRepository adminRepository;
     @Autowired
     private TokenProvider tokenProvider;
 
@@ -32,13 +36,23 @@ class TokenProviderTest {
                 .profileImg("http://localhost:8080/images")
                 .build();
 
+        Admin admin = Admin.builder()
+                .email("admin@naver.com")
+                .password("1234")
+                .build();
+
+
         Member saveMember = memberRepository.save(member);
+        Admin saveAdmin = adminRepository.save(admin);
 
         UserPrincipal userPrincipal = UserPrincipal.create(saveMember);
+        UserPrincipal adminPrincipal = UserPrincipal.create(saveAdmin);
 
         String token = tokenProvider.createToken(userPrincipal);
+        String adminToken = tokenProvider.createToken(adminPrincipal);
 
         System.out.println(token);
+        System.out.println(adminToken);
 
         /*
          * 서버를 실행할 때 마다 토큰 값이 달라지는 것을 알고 있어야 합니다
